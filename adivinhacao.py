82ff6779b180235cf841475ca8ba43090216c330
Gileade de Jesus <gjes@cesar.school>
2021-12-30 15:15:53 -0300

    jogo advinhaçao

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,9 +4,8 @@ print("*********************************")
 
 numero_secreto = 42
 total_tentativas = 3
-rodada = 1
 
-while(rodada <= total_tentativas):
+for rodada in range(1, total_tentativas):
     print("Tentativa: {0} de {1}".format(rodada, total_tentativas))
     chute = int(input("Digite o seu numero: "))
 
@@ -23,7 +22,5 @@ while(rodada <= total_tentativas):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
-
-    rodada = rodada +1
 
 print("Fim do jogo")
