6ee06904e4a3bcdeaf2573949a6a22765d83dfab
Gileade de Jesus <gjes@cesar.school>
2021-12-30 16:35:39 -0300

    Implementação das funções break e continue

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,17 +6,18 @@ numero_secreto = 42
 total_tentativas = 3
 
 for rodada in range(1, total_tentativas+1):
-    print("Tentativa: {0} de {1}".format(rodada, total_tentativas))
-    chute = int(input("Digite o seu numero: "))
-
+    chute = int(input("Tentativa: {0} de {1}.\nDigite um numero entre 1 e 100: ".format(rodada, total_tentativas)))
     print("Você digitou ", chute)
-
+    if(chute < 1 or chute >100):
+        print("Você deve digitar um numero entre 1 e 100!")
+        continue
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
     menor   = chute < numero_secreto
 
     if (acertou):
         print("Parabéns!! Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
