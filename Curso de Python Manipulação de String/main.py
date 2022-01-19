b591aed8ce42442b2982854ddb69d43c03f5e33d
Gileade de Jesus <gjes@cesar.school>
2022-01-19 17:31:37 -0300

    manipulaçao de string

--- Curso de Python Manipulação de String/main.py
+++ Curso de Python Manipulação de String/main.py
@@ -1,9 +1,27 @@
-argumento = "www.bytebank.com.br/cambio?moedaorigem=real"
+from ExtratorArgumentosUrl import ExtratorArgumentosUrl
 
-index = argumento.find("?")
-sub_string = argumento[index + 1 : ]
-print(sub_string)
+url = "moedaorigem=moedadestino&moedadestino=dolar"
+argumento = ExtratorArgumentosUrl(url)
+print(f"Url validada: {ExtratorArgumentosUrl.url_valida(url)}")
+moeda_origem, moeda_destino = argumento.extrair_argumentos()
+print(f"Moeada de Origem: {moeda_origem} e Moeda de Destino: {moeda_destino}")
 
-sub_string2 = argumento.split("?")
-print(sub_string2)
+
+
+
+
+
+
+
+# argumento = "www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=700"
+# index = argumento.find("?")
+# sub_string = argumento[index + 1 : ]
+# print(sub_string)
+# sub_string2 = argumento.split("?")
+# print(sub_string2)
+
+
+# index = url.find("moedadestino")
+# sub_string = url[index +len("moedadestino") +1:]
+# print(sub_string)
 
